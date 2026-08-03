8b7327254095db5f9ed9958364e49ee9ddbdace6
MindofGhost <vis.alex11@gmail.com>
2026-08-04 02:20:02 +0300

    ADD: system DNS fallback for vk-turn

--- core/src/main/golang/native/app/dns.go
+++ core/src/main/golang/native/app/dns.go
@@ -2,8 +2,14 @@ package app
 
 import (
 	"strings"
+	"sync"
 
 	"github.com/metacubex/mihomo/dns"
+)
+
+var (
+	systemDNSMu sync.RWMutex
+	systemDNS   []string
 )
 
 func NotifyDnsChanged(dnsList string) {
@@ -11,6 +17,18 @@ func NotifyDnsChanged(dnsList string) {
 	if len(dnsList) > 0 {
 		addr = strings.Split(dnsList, ",")
 	}
+
+	systemDNSMu.Lock()
+	systemDNS = append(systemDNS[:0], addr...)
+	systemDNSMu.Unlock()
+
 	dns.UpdateSystemDNS(addr)
 	dns.FlushCacheWithDefaultResolver()
+}
+
+func SystemDNS() []string {
+	systemDNSMu.RLock()
+	defer systemDNSMu.RUnlock()
+
+	return append([]string(nil), systemDNS...)
 }
